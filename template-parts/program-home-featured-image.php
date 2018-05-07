687f28ff61cfd2876c8609e05ef0bb7b7014efda
ksascomm <ksasweb@jhu.edu>
2018-05-07 14:19:25 -0400

    increase height of program-home featured-hero

--- template-parts/program-home-featured-image.php
+++ template-parts/program-home-featured-image.php
@@ -1,4 +1,4 @@
-<header class="featured-hero"  hide-for-print" role="banner" data-interchange="[<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, small], [<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, medium], [<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, large], [<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, xlarge]" aria-label="Featured Image">
+<header class="featured-hero program-home hide-for-print" role="banner" data-interchange="[<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, small], [<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, medium], [<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, large], [<?php echo get_post_meta($post->ID, 'ecpt_slideimage', true); ?>, xlarge]" aria-label="Featured Image">
    
         <figcaption class="orbit-caption" aria-hidden="true">
           <h1><?php the_title(); ?></h1>        
