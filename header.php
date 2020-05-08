9aeb9db786bf9a71435ef67146488fd4b800caa7
ksascomm <ksascomm@gmail.com>
2020-05-08 07:48:06 -0400

    Remove old favicons

--- header.php
+++ header.php
@@ -17,31 +17,7 @@
 		<meta name="date" content="<?php the_modified_date(); ?>" />
 		<title><?php create_page_title_grll(); ?></title>
 
-		<!-- Icons & Favicons -->
-        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-57x57.png">
-	    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-60x60.png">
-	    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-72x72.png">
-	    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-76x76.png">
-	    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-114x114.png">
-	    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-120x120.png">
-	    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-144x144.png">
-	    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-152x152.png">
-	    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-180x180.png">
-	    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/android-icon-192x192.png">
-	    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/favicon-32x32.png">
-	    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/favicon-96x96.png">
-	    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/favicon-16x16.png">
-
-		<!--[if lte IE 9]>
-	 		<div class="callout alert">
-				<?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.'); ?>	
-			</div>		
-    	<![endif]-->
-
-		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/dist/assets/images/icons/apple-icon-120x120.png">
-    	<meta name="theme-color" content="#121212">
-
+		<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/dist/assets/images/favicons/browserconfig.xml" />
 		<?php wp_head(); ?>
 		
 		<?php get_template_part( 'template-parts/analytics' ); ?>
