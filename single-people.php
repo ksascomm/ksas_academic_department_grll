3667444441aca17145cbbe3819001d22d27a5040
ksascomm <ksasweb@jhu.edu>
2022-02-02 10:28:03 -0500

    Reformat code

--- single-people.php
+++ single-people.php
@@ -5,8 +5,9 @@
  * @package KSASAcademicDepartment
  * @since   KSASAcademicDepartment 1.0.0
  */
+
 get_header(); ?>
-  <?php
+	<?php
 	$program_slug = get_the_program_slug( $post );
 	$program_name = get_the_program_name( $post );
 	?>
@@ -40,109 +41,107 @@ get_header(); ?>
 					<?php endif; ?>
 						<div class="small-12 medium-8 cell">
 						<?php if ( get_post_meta( $post->ID, 'ecpt_position', true ) ) : ?>
-							<h2><?php echo get_post_meta( $post->ID, 'ecpt_position', true ); ?></h2>
+							<h2><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_position', true ) ); ?></h2>
+						<?php endif; ?>
+						<?php if ( get_post_meta( $post->ID, 'ecpt_degrees', true ) ) : ?>
+								<h3><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_degrees', true ) ); ?></h3>
 						<?php endif; ?>
 						<p class="listing">
-							<span class="fa fa-flag"></span> Program: <?php echo $program_name; ?><br>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_office', true ) ) : ?>
-							<span class="fa fa-map-marker-alt" aria-hidden="true"></span> <?php echo get_post_meta( $post->ID, 'ecpt_office', true ); ?><br>
-							<?php endif; ?>
-
-							<?php if ( get_post_meta( $post->ID, 'ecpt_hours', true ) ) : ?>
-							<span class="fa fa-calendar" aria-hidden="true"></span> <?php echo get_post_meta( $post->ID, 'ecpt_hours', true ); ?><br>
-							<?php endif; ?>
-
-							<?php if ( get_post_meta( $post->ID, 'ecpt_phone', true ) ) : ?>
-							<span class="fa fa-phone-square" aria-hidden="true"></span> <?php echo get_post_meta( $post->ID, 'ecpt_phone', true ); ?><br>
-							<?php endif; ?>
-
-							<?php if ( get_post_meta( $post->ID, 'ecpt_fax', true ) ) : ?>
-							<span class="fa fa-fax" aria-hidden="true"></span>  <?php echo get_post_meta( $post->ID, 'ecpt_fax', true ); ?><br>
-							<?php endif; ?>
-
+							<span class="fa fa-flag"></span> Program: <?php echo esc_html( $program_name ); ?><br>
 							<?php
 							if ( get_post_meta( $post->ID, 'ecpt_email', true ) ) :
-								$email = get_post_meta( $post->ID, 'ecpt_email', true );
+									$email = get_post_meta( $post->ID, 'ecpt_email', true );
 								?>
-							<span class="fa fa-envelope" aria-hidden="true"></span> <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;<?php echo email_munge( $email ); ?>">
-
-								<?php echo email_munge( $email ); ?> </a><br>
-							<?php endif; ?>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_cv', true ) ) : ?>
-						<span class="fas fa-file-pdf" aria-hidden="true"></span> <a href="<?php echo get_post_meta( $post->ID, 'ecpt_cv', true ); ?>">Curriculum Vitae</a><br>
-							<?php endif; ?>
-
-							<?php if ( get_post_meta( $post->ID, 'ecpt_website', true ) ) : ?>
-						<span class="fa fa-globe" aria-hidden="true"></span> <a href="<?php echo get_post_meta( $post->ID, 'ecpt_website', true ); ?>" target="_blank">Personal Website</a><br>
-							<?php endif; ?>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_lab_website', true ) ) : ?>
-						<span class="fa fa-globe" aria-hidden="true"></span> <a href="<?php echo get_post_meta( $post->ID, 'ecpt_lab_website', true ); ?>" target="_blank">Group/Lab Website</a><br>
-							<?php endif; ?>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_google_id', true ) ) : ?>
-						<span class="fab fa-google"></span> <a href="http://scholar.google.com/citations?user=<?php echo get_post_meta( $post->ID, 'ecpt_google_id', true ); ?>" target="_blank">Google Scholar Profile</a><br>
-							<?php endif; ?>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_microsoft_id', true ) ) : ?>
-						<span class="fab fa-windows"></span> <a href="https://academic.microsoft.com/#/detail/<?php echo get_post_meta( $post->ID, 'ecpt_microsoft_id', true ); ?>" target="_blank"> Microsoft Academic Profile</a>
-							<?php endif; ?>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_orcid_id', true ) ) : ?>
-						<a href="http://orcid.org/<?php echo get_post_meta( $post->ID, 'ecpt_orcid_id', true ); ?>" target="_blank"><span class="fas fa-user"></span> ORCID Profile</a>
-							<?php endif; ?>
-							<?php if ( get_post_meta( $post->ID, 'ecpt_twitter', true ) ) : ?>
-						<span class="fab fa-twitter"></span> <a href="https://twitter.com/<?php echo get_post_meta( $post->ID, 'ecpt_twitter', true ); ?>" target="_blank"> @<?php echo get_post_meta( $post->ID, 'ecpt_twitter', true ); ?></a>
-							<?php endif; ?>
+								<span class="fa fa-envelope" aria-hidden="true"></span>
+									<?php if ( function_exists( 'email_munge' ) ) : ?>
+									<a class="munge" href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;<?php echo email_munge( $email ); ?>">
+										<?php echo email_munge( $email ); ?>
+									</a>
+									<?php else : ?>
+									<a href="<?php echo esc_url( 'mailto:' . $email ); ?>"><?php echo esc_html( $email ); ?></a>
+									<?php endif; ?>
+									<br>
+								<?php endif; ?>
+								<?php if ( get_field( 'ecpt_cv' ) ) : ?>
+								<span class="fas fa-file-pdf" aria-hidden="true"></span> <a href="<?php the_field( 'ecpt_cv' ); ?>">Curriculum Vitae</a><br>
+								<?php endif; ?>
+								<?php
+								$file = get_field( 'cv_file' );
+								if ( $file ) :
+									?>
+								<span class="fas fa-file-pdf" aria-hidden="true"></span> <a href="<?php echo esc_url( $file['url'] ); ?>">Curriculum Vitae</a><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_office', true ) ) : ?>
+								<span class="fa fa-map-marker-alt" aria-hidden="true"></span> <?php echo esc_html( get_post_meta( $post->ID, 'ecpt_office', true ) ); ?><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_hours', true ) ) : ?>
+								<span class="fa fa-calendar" aria-hidden="true"></span> <?php echo esc_html( get_post_meta( $post->ID, 'ecpt_hours', true ) ); ?><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_phone', true ) ) : ?>
+								<span class="fa fa-phone-square" aria-hidden="true"></span> <?php echo esc_html( get_post_meta( $post->ID, 'ecpt_phone', true ) ); ?><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_website', true ) ) : ?>
+								<span class="fa fa-globe" aria-hidden="true"></span> <a href="<?php echo esc_url( get_post_meta( $post->ID, 'ecpt_website', true ) ); ?>" target="_blank">Personal Website</a><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_lab_website', true ) ) : ?>
+								<span class="fa fa-globe" aria-hidden="true"></span> <a href="<?php echo esc_url( get_post_meta( $post->ID, 'ecpt_lab_website', true ) ); ?>" target="_blank">Group/Lab Website</a><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_google_id', true ) ) : ?>
+								<span class="fab fa-google"></span> <a href="http://scholar.google.com/citations?user=<?php echo esc_html( get_post_meta( $post->ID, 'ecpt_google_id', true ) ); ?>" target="_blank">Google Scholar Profile</a><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_orcid_id', true ) ) : ?>
+								<span class="fas fa-user"></span> <a href="http://orcid.org/<?php echo esc_html( get_post_meta( $post->ID, 'ecpt_orcid_id', true ) ); ?>" target="_blank">ORCID Profile</a><br>
+								<?php endif; ?>
+								<?php if ( get_post_meta( $post->ID, 'ecpt_twitter', true ) ) : ?>
+								<span class="fab fa-twitter"></span> <a href="https://twitter.com/<?php echo esc_html( get_post_meta( $post->ID, 'ecpt_twitter', true ) ); ?>" target="_blank"> @<?php echo esc_html( get_post_meta( $post->ID, 'ecpt_twitter', true ) ); ?></a>
+								<?php endif; ?>
 						</p>
 					</div>
 				</div>
 					<?php if ( has_term( '', 'role' ) && ! has_term( 'job-market-candidate', 'role' ) ) : ?>
-					<ul class="tabs margin10" data-tabs id="profile-tabs">
+						<ul class="tabs margin10" data-tabs id="profile-tabs">
 						<?php if ( get_post_meta( $post->ID, 'ecpt_bio', true ) ) : ?>
-						<li class="tabs-title is-active"><a href="#bioTab">Biography</a></li>
-						<?php endif; ?>
+					<li class="tabs-title is-active"><a href="#bioTab">Biography</a></li>
+					<?php endif; ?>
 						<?php if ( get_post_meta( $post->ID, 'ecpt_research', true ) ) : ?>
-						<li class="tabs-title"><a href="#researchTab">Research</a></li>
-						<?php endif; ?>
+					<li class="tabs-title"><a href="#researchTab">Research</a></li>
+					<?php endif; ?>
 
 						<?php if ( get_post_meta( $post->ID, 'ecpt_teaching', true ) ) : ?>
-						<li class="tabs-title"><a href="#teachingTab">Teaching</a></li>
-						<?php endif; ?>
+					<li class="tabs-title"><a href="#teachingTab">Teaching</a></li>
+					<?php endif; ?>
 
 						<?php if ( get_post_meta( $post->ID, 'ecpt_publications', true ) ) : ?>
-						<li class="tabs-title"><a href="#publicationsTab">Publications</a></li>
-						<?php endif; ?>
+					<li class="tabs-title"><a href="#publicationsTab">Publications</a></li>
+					<?php endif; ?>
 						<?php if ( get_post_meta( $post->ID, 'ecpt_books_cond', true ) == 'on' ) : ?>
-						<li class="tabs-title"><a href="#booksTab">Books</a></li>
-						<?php endif; ?>
+					<li class="tabs-title"><a href="#booksTab">Books</a></li>
+					<?php endif; ?>
 						<?php if ( get_post_meta( $post->ID, 'ecpt_extra_tab_title', true ) ) : ?>
-						<li class="tabs-title"><a href="#extraTab"><?php echo get_post_meta( $post->ID, 'ecpt_extra_tab_title', true ); ?></a></li>
-						<?php endif; ?>
+					<li class="tabs-title"><a href="#extraTab"><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_extra_tab_title', true ) ); ?></a></li>
+					<?php endif; ?>
 						<?php if ( get_post_meta( $post->ID, 'ecpt_extra_tab_title2', true ) ) : ?>
-						<li class="tabs-title"><a href="#extra2Tab"><?php echo get_post_meta( $post->ID, 'ecpt_extra_tab_title2', true ); ?></a></li>
-						<?php endif; ?>
-					</ul>
+					<li class="tabs-title"><a href="#extra2Tab"><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_extra_tab_title2', true ) ); ?></a></li>
+					<?php endif; ?>
+				</ul>
 
 					<div class="tabs-content people-content" data-tabs-content="profile-tabs">
 						<?php if ( get_post_meta( $post->ID, 'ecpt_bio', true ) ) : ?>
 						<div class="tabs-panel is-active" id="bioTab" itemprop="articleBody">
-							<?php echo get_post_meta( $post->ID, 'ecpt_bio', true ); ?>
+							<?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_bio', true ) ); ?>
 						</div>
 						<?php endif; ?>
 
 						<?php if ( get_post_meta( $post->ID, 'ecpt_research', true ) ) : ?>
-						<div class="tabs-panel" id="researchTab"><?php echo get_post_meta( $post->ID, 'ecpt_research', true ); ?></div>
+						<div class="tabs-panel" id="researchTab"><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_research', true ) ); ?></div>
 						<?php endif; ?>
 
 						<?php if ( get_post_meta( $post->ID, 'ecpt_teaching', true ) ) : ?>
-						<div class="tabs-panel" id="teachingTab"><?php echo get_post_meta( $post->ID, 'ecpt_teaching', true ); ?></div>
+						<div class="tabs-panel" id="teachingTab"><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_teaching', true ) ); ?></div>
 						<?php endif; ?>
 
 						<?php if ( get_post_meta( $post->ID, 'ecpt_publications', true ) ) : ?>
-						<div class="tabs-panel" id="publicationsTab">
-							<?php
-							if ( get_post_meta( $post->ID, 'ecpt_publications', true ) ) :
-								echo get_post_meta( $post->ID, 'ecpt_publications', true );
-							endif;
-							?>
-						</div>
+						<div class="tabs-panel" id="publicationsTab"><?php echo wp_kses_post( get_post_meta( $post->ID, 'ecpt_publications', true ) ); ?></div>
 						<?php endif; ?>
 						<?php
 						if ( get_post_meta( $post->ID, 'ecpt_books_cond', true ) == 'on' ) :
@@ -150,11 +149,11 @@ get_header(); ?>
 						endif;
 						?>
 						<?php if ( get_post_meta( $post->ID, 'ecpt_extra_tab', true ) ) : ?>
-						<div class="tabs-panel"  id="extraTab"><?php echo get_post_meta( $post->ID, 'ecpt_extra_tab', true ); ?></div>
+						<div class="tabs-panel"  id="extraTab"><?php echo apply_filters( 'the_content', wp_kses_post( get_post_meta( $post->ID, 'ecpt_extra_tab', true ) ) ); ?></div>
 						<?php endif; ?>
 
 						<?php if ( get_post_meta( $post->ID, 'ecpt_extra_tab2', true ) ) : ?>
-						<div class="tabs-panel" id="extra2Tab"><?php echo get_post_meta( $post->ID, 'ecpt_extra_tab2', true ); ?></div>
+						<div class="tabs-panel" id="extra2Tab"><?php echo apply_filters( 'the_content', wp_kses_post( get_post_meta( $post->ID, 'ecpt_extra_tab2', true ) ) ); ?></div>
 						<?php endif; ?>
 					</div>
 				<?php endif; ?>
@@ -166,7 +165,7 @@ get_header(); ?>
 		<div class="sidebar">
 			<aside class="sidebar-menu-area" aria-labelledby="sidebar-navigation-people">
 				<div class="sidebar-menu">
-					<h1 class="sidebar-menu-title" id="sidebar-navigation-people">Explore all <a href="<?php echo get_home_url(); ?>/people/" aria-label="Sidebar Menu: People Heading">People</a></h1>
+					<h1 class="sidebar-menu-title" id="sidebar-navigation-people">Explore all <a href="<?php echo esc_url( get_home_url() ); ?>/people/" aria-label="Sidebar Menu: People Heading">People</a></h1>
 					<?php
 						wp_nav_menu(
 							array(
@@ -182,8 +181,8 @@ get_header(); ?>
 			</aside>
 			<?php if ( has_term( '', 'role' ) && ! is_single( 'earle-havens' ) ) : ?>
 			<aside class="sidebar-menu-area" aria-labelledby="sidebar-navigation">
-				<div class="sidebar-menu <?php echo $program_slug; ?>">
-					<h1 class="sidebar-menu-title" id="sidebar-navigation">Also in <a href="<?php echo site_url( '/' ) . $program_slug; ?>" aria-label="<?php echo $program_name; ?> Program Menu Heading"><?php echo $program_name; ?> Program</a></h1>
+				<div class="sidebar-menu <?php echo esc_html( $program_slug ); ?>">
+					<h1 class="sidebar-menu-title" id="sidebar-navigation">Also in <a href="<?php echo esc_url( site_url( '/' ) . $program_slug ); ?>" aria-label="<?php echo esc_html( $program_name ); ?> Program Menu Heading"><?php echo esc_html( $program_name ); ?> Program</a></h1>
 					<?php
 						wp_nav_menu(
 							array(
@@ -196,7 +195,7 @@ get_header(); ?>
 					?>
 				</div>
 			</aside>
-			<?php endif; ?>   
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
