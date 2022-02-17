1a4d7c81dccd399f8b606c1d80c9f853f5d458b8
ksascomm <ksasweb@jhu.edu>
2022-02-17 13:33:00 -0500

    Remove unused file

--- gutenberg-editor/editor.js
+++
@@ -1,7 +0,0 @@
-// Used to remove block options in editor
-
-// Remove outline & Square Block
-wp.domReady( () => {
-	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
-} );
